95929c20fa48bc16c9a00c163561b74785df3b10
littlestone0806 <1039398195@qq.com>
2023-05-08 15:55:56 +0800

    修改了虚拟路径

--- 04 【推理】摄像头.bat
+++ 04 【推理】摄像头.bat
@@ -1,2 +1,2 @@
-env\python.exe env_install\env\Scripts\yolo.exe predict model=runs/detect/train/weights/best.pt source=0 show=True save=False
+env\python.exe env\Scripts\yolo.exe predict model=runs/detect/train/weights/best.pt source=0 show=True save=False
 pause
