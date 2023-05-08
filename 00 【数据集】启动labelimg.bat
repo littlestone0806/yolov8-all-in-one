fc10782c0c8cef85c1e19ed842e4ede864615d64
littlestone0806 <1039398195@qq.com>
2023-05-08 15:29:00 +0800

    Update 00 【数据集】启动labelimg.bat

--- 00 【数据集】启动labelimg.bat
+++ 00 【数据集】启动labelimg.bat
@@ -1,2 +1,2 @@
-env\python.exe env_install\env\Scripts\labelimg.exe datasets\VOCdevkit\JPEGImages datasets\VOCdevkit\txt\classes.txt
+env\python.exe env\Scripts\labelimg.exe datasets\VOCdevkit\JPEGImages datasets\VOCdevkit\txt\classes.txt
 pause
